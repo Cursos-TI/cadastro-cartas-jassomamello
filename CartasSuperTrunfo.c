22b4d7dd99e48673865174720b11ce448cda944f
jassomamello <jassom.a.mello@gmail.com>
2025-10-17 01:29:37 +0000

    Criei e executei código do Desafio Novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,31 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  int  pontos;
+  float populacao, area, PIB;
+  char nome[50];
 
   // Área para entrada de dados
+  printf("Digite o nome da cidade: ");
+  scanf("%s", nome);
+
+  printf("Digite a população da cidade: ");
+  scanf("%f", &populacao);
+
+  printf("Digite a área da cidade: ");
+  scanf("%f", &area);
+
+  printf("Digite o PIB da cidade: ");
+  scanf("%f", &PIB);
+
+  printf("Digite a quantidade de pontos turísticos da cidade: ");
+  scanf("%d", &pontos);
 
   // Área para exibição dos dados da cidade
+
+  printf("A cidade escolhida é %s\nSua população é de, aproximadamente, %.1f milhões de habitantes \n", nome, populacao);
+  printf("A área desta cidade é de %.2f km² \nSeu PIB é de R$ %.2f bilhões \n", area, PIB);
+  printf("Possui mais de %d pontos turísticos \n", pontos);
 
 return 0;
 } 
